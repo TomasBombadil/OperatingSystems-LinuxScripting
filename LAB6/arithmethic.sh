8348bd4fe005e1b950c6b5ae94b9e0720fc05f5a
root <root@localhost.localdomain>
2018-11-27 17:57:22 +0000

    Work in progress

--- LAB6/arithmethic.sh
+++ LAB6/arithmethic.sh
@@ -10,18 +10,43 @@ operation () {
 	
 	sub="$(echo $op1 | bc)"
 	add="$(echo $op2 | bc)"
+	
+if [ $add -ne 0 ]; then
 	div="$sub/$add"
 
 	result="$(echo $div | bc -l)"
-	echo "Z1 result: $result"
+	printf "(\$1-\$2)/(\$1+\$2): $result\n"
+
+else 
+	printf "\nNominal cannot be zero!\n"
+	exit -1
+fi
 }
+#Z2
+recursiveFibbonacci () {
+	funReq=1
+	num1=$1;
+	num2=		
+	if [ $funReq-eq $#]; then
+
+		if [ $num1 -le 0 ]; then
+			printf 0
+		elif [ $num1
+	else
+		printf "\nFunction required arguments: $required  | Provided args: $provided \n"
+	exit -1
+fi
+
+}
+
 
 # MAIN
 if [ $required -eq $provided ]; then
 #Z1
-	echo "starting\n"
-	operation $1 $2	
+	#operation $1 $2	
+#Z2
+	recursiveFibbonacci $1 
 else
 	printf "\nRequired arguments: $required  | Provided args: $provided \n"
-
+	exit -1
 fi
